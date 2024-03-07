88382d40a96fee2ccaa3dd230e2a64060a363062
nyaps12 <raffylimbs17@gmail.com>
2024-03-07 13:14:19 +0800

    major fix login

--- resources/views/user/map.blade.php
+++ resources/views/user/map.blade.php
@@ -15,7 +15,7 @@
 
         <div class="d-flex align-items-center justify-content-between">
             <a href="index.html" class="logo d-flex align-items-center">
-                <img src="assets/img/image.png" alt="">
+                <img src="assets/img/bbox.png" alt="">
                 <span class="d-none d-lg-block">Driver Portal</span>
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>
@@ -181,7 +181,7 @@
                 <li class="nav-item dropdown pe-3">
 
                     <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                        <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+                        <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle border">
                         <span class="d-none d-md-block dropdown-toggle ps-2">{{$user->name}}</span>
                     </a><!-- End Profile Iamge Icon -->
 
